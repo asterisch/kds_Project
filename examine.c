a4041f861803b66121e05f69e71cc6b25caaf25c
KostasMp <mpenos.ks@gmail.com>
2016-04-02 18:48:30 +0300

    Beautify the code for readability

--- examine.c
+++ examine.c
@@ -8,34 +8,39 @@
 
 int check_input(int argc,char *argv[])
 {
-  //If input arguments are not as much as meant to be, the algorithm ends indicating it's usage.
-  if (argc<6 || argc>6)
-  {
-    printf("[-]Usage: ./examine [max_collisions] [max_exec_time] [input_file] [Threads] [Processes] \nUse \"-1\": for no boundies \n");
-    if (argc==2) if (!strcmp(argv[1],"--help")) printf("max_collisions: Maximum number of collisions\nmax_exec_time: Maximum execution time\ninput_file: Filename to examine\nThreads: Number of threads to use\nProcesses: Number of Processes to use.\n" );
-    return 1;
-  }
-  return 0;
+	if (argc<6 || argc>6) // If input arguments are not as much as meant to be, the algorithm ends indicating it's usage.
+	{
+		printf("[-]Usage: ./examine [max_collisions] [max_exec_time] [input_file] [Threads] [Processes] \nUse \"-1\": for no boundies \n");
+		if (argc==2) if (!strcmp(argv[1],"--help")) printf("max_collisions: Maximum number of collisions\nmax_exec_time: Maximum execution time\ninput_file: Filename to examine\nThreads: Number of threads to use\nProcesses: Number of Processes to use.\n" );
+		return 1;
+	}
+	return 0;
 }
 
 FILE *check_input_file(char *filename)
 {
-   FILE *temp = fopen(filename,"r");
-   if (!temp)
-   {
-     printf("[!]Input file does not exist.\nExiting...\n");
-     return NULL;
-   }
-   return temp;
+	FILE *temp = fopen(filename,"r");
+	if (!temp)
+	{
+		printf("[!]Input file does not exist.\nExiting...\n");
+		return NULL;
+	}
+	return temp;
 } 
 
 int main(int argc,char * argv[])
 {
-  if (check_input(argc,argv)==1) return 1;
-  FILE *input = check_input_file(argv[3]);
-  if  (!input) return 1;
-  int coll=atoi(argv[1]);//,etime=argv[2],threads=argv[4],proc=argv[5];
-  fclose(input);
-  printf("[+]Done! \n" );
-  return 0;
+	if (check_input(argc,argv)==1)
+	{
+		return 1;
+	}
+	FILE *input = check_input_file(argv[3]);
+	if  (!input)
+	{
+		return 1;
+	}
+	int coll=atoi(argv[1]);											//,etime=argv[2],threads=argv[4],proc=argv[5];
+	fclose(input);
+	printf("[+]Done! \n" );
+	return 0;
 }
--- generator.c
+++ generator.c
@@ -1,65 +1,69 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
 int check_input(int argc)
 {
-  //If input arguments are not as much as meant to be, the algorithm ends indicating it's usage.
-  if (argc<3 || argc>3)
-  {
-    printf("Usage: ./generator [output_filename] [number_of_coords] \n");
-    return 1;
-  }
-  return 0;
+	if (argc<3 || argc>3)	// If input arguments are not as much as meant to be, the algorithm ends indicating it's usage.
+	{
+		printf("Usage: ./generator [output_filename] [number_of_coords] \n");
+		return 1;
+	}
+	return 0;
 }
+
 FILE *create_file(char *argv1)
 {
-    FILE *fp;
+	FILE *fp;
     char overwrite='y';
-    fp =fopen(argv1,"r");//checks if file already exists in the folder
+    fp =fopen(argv1,"r");	// Check if file already exists in the folder
     if (fp)
     {
-      do{
-        printf("[!]Warning :File \"%s\" already exists.\nOverwrite? (y/n):",argv1);
-        overwrite=getc(stdin);
-      }while (overwrite!='y' && overwrite!='n');
-      fclose(fp);
+		do
+		{
+			printf("[!]Warning :File \"%s\" already exists.\nOverwrite? (y/n):",argv1);
+			overwrite=getc(stdin);
+		}while (overwrite!='y' && overwrite!='n');
+		fclose(fp);
     }
-    if (overwrite=='y')//write file either already exist or not
+    if (overwrite=='y')		// Write file either already exist or not
     {
       return fopen(argv1,"w");
     }
-    return NULL;//do not overwrite anything
+    return NULL;			// Do not overwrite anything
 }
+
 void write_matrix(FILE *out,unsigned int coords)
 {
-  int utime;
-  long int ltime;
-  unsigned int count,j;
-  ltime=time(NULL);
-  utime= (unsigned int) ltime/2;
-  srand(utime);
-  for (count=0;count<coords;count++)
-  {
-    for (j=0;j<3;j++)
-    {
-        fprintf(out, "%.6f ", (float) 34*rand() / (RAND_MAX-1 ) );
-    }
-    fprintf(out,"\n");
-  }
+	int utime;
+	long int ltime;
+	unsigned int count,j;
+	ltime=time(NULL);
+	utime= (unsigned int) ltime/2;
+	srand(utime);	
+	for (count=0;count<coords;count++)
+	{
+		for (j=0;j<3;j++)
+		{
+			fprintf(out, "%.6f ", (float) 34*rand() / (RAND_MAX-1 ) );
+		}
+		fprintf(out,"\n");
+	}
 }
+
 int main(int argc,char *argv[])
 {
-   if (check_input(argc)==1) return 1;//abuse test
-   FILE *file = create_file(argv[1]);//file handling
-   unsigned int nocords=atoi(argv[2]);//number of coordinates
-   if (!file)
-   {
-     printf("Nothing to write..\nExiting...\n");
-     return 1;
-   }
-   printf("[-]Working...\n");
-   write_matrix(file,nocords);//write matrix to file
-   fclose(file);
-   printf("[+]Done! \n" );
-   return 0;
- }
+	if (check_input(argc)==1) return 1;		// Abuse test
+	FILE *file = create_file(argv[1]);		// File handling
+	unsigned int nocords=atoi(argv[2]);		// Number of coordinates
+	if (!file)
+	{
+		printf("Nothing to write..\nExiting...\n");
+		return 1;
+	}
+	printf("[-]Working...\n");
+	write_matrix(file,nocords);				// Write matrix to file
+	fclose(file);
+	printf("[+]Done! \n" );
+	return 0;
+}
